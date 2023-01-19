9533ba48031b36b07bf9d9ae6cebcef4e1c58fc6
Amanuel Zeredawit <amanuelsimon16@gmail.com>
2023-01-19 17:00:53 +0100

    pipeline updated

--- preprocessing/pca.py
+++ preprocessing/pca.py
@@ -1,5 +1,22 @@
 from sklearn.decomposition import PCA
 import pandas as pd
+from sklearn.cluster import KMeans
+
+
+def determine_n_clusters(data, features):
+    n_cluster = 5
+    k_max = 10
+    from sklearn.metrics import silhouette_score
+
+    sil = []  
+   # dissimilarity would not be defined for a single cluster, thus, minimum number of clusters should be 2
+    for k in range(2, k_max+1):
+        kmeans = KMeans(n_clusters = k).fit(data[features])
+        labels = kmeans.labels_
+        sil.append(silhouette_score(data[features], labels, metric = 'euclidean'))
+
+    return sil
+
 
 def apply_pca(data,features,n):
     """apply pca to selected features
