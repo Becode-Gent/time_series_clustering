27c1328b5b029e9351d8e818f911f69fc211666f
Louis de Viron <louis@datatext.eu>
2023-02-08 15:41:32 +0100

    Implement suggestions from the client

--- preprocessing/get_path.py
+++ preprocessing/get_path.py
@@ -1,5 +1,6 @@
 import os
 
+
 def get_file_name(file_type):
     """get_file_name get the list of file names with .csv extension in the directory
 
@@ -8,13 +9,13 @@ def get_file_name(file_type):
     """
 
     if file_type == "labeled":
-        path = r"D:\Arai4_Projects\spinwise_project\spinewise_amanuel\ML_Project\data\labeled\movement_scription"
+        path = r"D:\Arai4_Projects\unclassified_clustering_project\unclassified_clustering\ML_Project\data\labeled\movement_scription"
     else:
-        path = r"D:\Arai4_Projects\spinwise_project\spinewise_amanuel\ML_Project\data\unlabeled"
+        path = r"D:\Arai4_Projects\unclassified_clustering_project\unclassified_clustering\ML_Project\data\unlabeled"
 
     filename_list = []
     ext = ('.csv')
     for file in os.listdir(path):
-        filename_list.append(file) 
-        
+        filename_list.append(file)
+
     return filename_list
